092cd83944734870cd0fd091ecdd6b0744d61c1b
fodilB <df_benali@esi.dz>
2019-02-08 10:20:55 +0100

    Add files via upload

---
+++ sqlfile.sql
@@ -0,0 +1,72 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.5.7
+-- Dumped by pg_dump version 9.5.7
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: STUDENTS; Type: TABLE; Schema: public; Owner: docker
+--
+
+CREATE TABLE "STUDENTS" (
+    "ID" bigint NOT NULL,
+    "NAME" text
+);
+
+
+ALTER TABLE "STUDENTS" OWNER TO docker;
+
+--
+-- Data for Name: STUDENTS; Type: TABLE DATA; Schema: public; Owner: docker
+--
+
+COPY "STUDENTS" ("ID", "NAME") FROM stdin;
+21702 	FODIL
+34342	ATTOUT
+34347	RANIA
+\.
+
+
+--
+-- Name: public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM postgres;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
